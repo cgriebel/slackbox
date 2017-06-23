4c7e070c38fb26884d6607cbc2e279cd9a03c079
Chris Griebel <chris.griebel.dev@gmail.com>
2017-06-23 00:46:40 -0400

    Test

--- app.js
+++ app.js
@@ -105,8 +105,8 @@ app.post('/store', function(req, res) {
             spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:0iq3MFEbuKTWJgdhwdOwXI'])
               .then(function(data) {
                 var message = 'Track added' + (process.env.SLACK_OUTGOING === 'true' ? ' by *' + req.body.user_name + '*' : '') + ': *' + track.name + '* by *' + track.artists[0].name + '*' + "\n " + 'spotify:track:' + track.id;
-                message = '\n[spotify:track:0iq3MFEbuKTWJgdhwdOwXI]' + ' : Hard coded:';
-                message += '\n[' + text + "] : Passed";
+                // message = '\n[spotify:track:0iq3MFEbuKTWJgdhwdOwXI]' + ' : Hard coded:';
+                // message += '\n[' + text + "] : Passed";
                 return slack(res, message);
               }, function(err) {
                 return slack(res, "Requested Track: [" + text + "] Error: [" + err.message + "]");
