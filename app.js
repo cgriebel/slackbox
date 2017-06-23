014ef0f8fc2ada08350a1db34c629f1cfaf080f0
Chris Griebel <chris.griebel.dev@gmail.com>
2017-06-23 01:19:01 -0400

    Test

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.post('/store', function(req, res) {
 
       spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, [text])
         .then(function(data) {
-          return slack(res, "Successfully added track to playlist - spotify:user:1288604005:playlist:42xjTUqLwlGC1uDyLUdxsG");
+          return slack(res, "Successfully added track to playlist - " + 
+          "spotify:user:" + process.env.SPOTIFY_USERNAME + ":playlist:" + process.env.SPOTIFY_PLAYLIST_ID);
         }, function(err) {
           return slack(res, err.message);
         });
